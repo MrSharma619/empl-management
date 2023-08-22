2f66027d95d1febda7350f1f4bd5cdf1b6fbcbf9
MrSharma619 <sh.manthan99@gmail.com>
2023-08-23 01:11:34 +0530

    functionality added

--- EmployeeManagementApp/CompanyManagement.cs
+++ EmployeeManagementApp/CompanyManagement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,52 +10,142 @@ namespace EmployeeManagementApp
 {
     internal class CompanyManagement
     {
-        public List<Employee> empList;
+        public List<Employee?>? empList;
 
         public CompanyManagement(string path, string path1)
         {
-            //TODO
+            getEmployeeFromFile(path, path1);
         }
 
-        public List<Employee> getEmployeeFromFile(string path, string path1)
+        public List<string> getPLforEmp(string path1, string empId)
         {
-            //TODO
-            return null;
+            List<string> programmingLanguages = new List<string>();
+
+            if (File.Exists(path1))
+            {
+                // Read a text file line by line.
+                string[] lines = File.ReadAllLines(path1);
+                foreach (string line in lines)
+                {
+                    var values = line.Split(',');
+                    if(values[0] == empId)
+                    {
+                        for(int i = 1; i < values.Length; i++)
+                        {
+                            programmingLanguages.Add(values[i]);
+                        }
+                    }
+                }
+            }
+
+            return programmingLanguages;
+        }
+
+        public List<Employee?> getEmployeeFromFile(string path, string path1)
+        {
+            empList = new List<Employee?>();
+
+            if (File.Exists(path))
+            {
+                // Read a text file line by line.
+                string[] lines = File.ReadAllLines(path);
+                foreach (string line in lines)
+                {
+                    Employee? e = null;
+                    var values = line.Split(',');
+
+                    List<string> programmingLanguages = getPLforEmp(path1, values[1]);
+
+                    if (values.Length == 6 && (values[4] == "AT" || values[4] == "MT"))
+                    {
+                        e = new Tester(values[1], values[2], Convert.ToInt32(values[5]), Convert.ToDouble(values[3]), values[4]);
+                    }
+                    else if (values.Length == 6)
+                    {
+                        e = new Developer(values[1], values[2], Convert.ToInt32(values[5]), values[3], programmingLanguages, Convert.ToInt32(values[4]));
+                    }
+                    else if (values.Length == 8 && values[5] == "L")
+                    {
+                        e = new TeamLeader(values[1], values[2], Convert.ToInt32(values[7]), values[3], programmingLanguages, Convert.ToInt32(values[4]), Convert.ToDouble(values[6]));
+                    }
+
+                    empList.Add(e);
+                }
+            }
+
+            return empList;
         }
 
         public List<Developer> getDevelopersByProgrammingLanguages(string pl)
         {
-            //TODO
-            return null;
+            List<Developer> devList = new List<Developer>();
+
+            foreach (var employee in empList)
+            {
+                if (employee is Developer developer && developer.getProgrammingLanguages().Contains(pl))
+                {
+                    devList.Add(developer);
+                }
+            }
+
+            return devList;
         }
 
         public List<Tester> getTestersHaveSalaryGreaterThan(double value)
         {
-            //TODO
-            return null;
+            List<Tester> testerList = new List<Tester>();
+
+            foreach (var employee in empList)
+            {
+                if (employee is Tester tester && (tester.getSalary() > value))
+                {
+                    testerList.Add(tester);
+                }
+            }
+
+            return testerList;
         }
 
         public Employee getEmployeeWithHigestSalary()
         {
-            //TODO
-            return null;
+            var maxSalary = empList.Max(r => r.getSalary());
+            return empList.Where(r => r.getSalary() == maxSalary).First();
         }
 
         public TeamLeader getLeaderWithMostEmployees()
         {
-            //TODO
-            return null;
+            List<TeamLeader> leaderList = new List<TeamLeader>();
+
+            var teamNameCounts = empList.OfType<Developer>()
+            .GroupBy(r => r.getTeamName())
+            .Select(group => new
+            {
+                TeamName = group.Key,
+                Count = group.Count()
+            })
+            .ToList();
+
+            var mostCommonTeam = teamNameCounts
+                .OrderByDescending(item => item.Count)
+                .FirstOrDefault();
+
+            string? teamName = mostCommonTeam?.TeamName;
+
+            return empList.OfType<TeamLeader>().First(r => r.getTeamName() == teamName);
         }
 
-        public List<Employee> sorted()
+        public List<Employee?>? sorted()
         {
-            //TODO
-            return null;
+            List<Employee?>? sortedEmpList = empList;
+
+            return sortedEmpList.OrderByDescending(r => r.getSalary())
+                .ThenBy(r => r.getEmpName())
+                .ToList();
         }
 
         public void printEmpList()
         {
-            //TODO
+            empList.ForEach(p => Console.WriteLine(p.toString()));
         }
 
         public Boolean writeFile(string path, List<Employee> list)
--- EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/Program.cs
@@ -1,8 +1,68 @@
-﻿public class Program
+﻿using EmployeeManagementApp;
+
+public class Program
 {
+    static readonly string textFileForEmployees = @"C:\Users\Manthan\source\repos\EmployeeManagementApp\EmployeeManagementApp\Input\ListOfEmployees.txt";
+
+    static readonly string textFileForPLs = @"C:\Users\Manthan\source\repos\EmployeeManagementApp\EmployeeManagementApp\Input\PLInfo.txt";
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("hi");
+        Console.WriteLine("Welcome");
+
+        CompanyManagement management = new CompanyManagement(textFileForEmployees, textFileForPLs);
+
+        while (true)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("1. Read all Employees and print to screen");
+            Console.WriteLine("2. Show staff proficient in a Programming Language");
+            Console.WriteLine("3. Show Tester with salary greater than 2500000");
+            Console.WriteLine("4. Show Employee wtih highest salary");
+            Console.WriteLine("5. Show Leader of the Team with most Employees");
+            Console.WriteLine("6. Sort Employees");
+            Console.WriteLine("7. Write file");
+            Console.WriteLine("8. Exit");
+            Console.WriteLine("\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            switch (option)
+            {
+                case 1: 
+                    management.printEmpList();
+                    break;
+                case 2:
+                    management.getDevelopersByProgrammingLanguages("C++")
+                        .ForEach(p => Console.WriteLine(p.toString()));
+                    break;
+                case 3:
+                    management.getTestersHaveSalaryGreaterThan(2500000)
+                        .ForEach(p => Console.WriteLine(p.toString()));
+                    break;
+                case 4:
+                    Console.WriteLine(management.getEmployeeWithHigestSalary().toString());
+                    break;
+                case 5:
+                    Console.WriteLine(management.getLeaderWithMostEmployees().toString());
+                    break;
+                case 6:
+                    management.sorted()
+                        .ForEach(p => Console.WriteLine(p.toString()));
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    return;
+                default:
+                    Console.WriteLine("Please try again!");
+                    break;
+            }
+        }
+
+        
     }
     
 }
